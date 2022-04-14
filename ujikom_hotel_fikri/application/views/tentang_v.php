157a6a4cfe7b909a7206bbc82e6281853a29ce0a
fikriamarullah <amarullahfikri114@gmail.com>
2022-04-14 14:19:24 +0700

    ujikom_hotel_fikri

---
+++ ujikom_hotel_fikri/application/views/tentang_v.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <center><h1>Tentang Kami</h1></center> <br
+   >
+<center>  <td>Hotel resik merupakan jenis hotel yang berada di kawasan pusat perkotaan dan </td></center>
+<br>
+<center>  <td>umumnya lebih banyak ditemukan di kota-kota besar seperti Bandung,Jakarta,Dan Kalimantan. Hotel resik biasanya berukuran </td></center>
+<br>
+<center>  <td>besar dengan gedung bertingkat dengan fasilitas-fasilitas bisnis dikarenakan tamu yang</td></center>
+<br>
+
+<center>  <td>menginap di hotel jenis ini kebanyakan merupakan business traveler. Hal ini membuat</td></center>
+<br>
+<center>  <td>jenis hotel ini juga kerap disebut sebagai business hotel atau hotel bisnis</td></center>
+</body>
+</html>
